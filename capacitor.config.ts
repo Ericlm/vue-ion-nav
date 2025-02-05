f3ff408aa007a3010d39e7dbb34d2ea65e998d3b
Éric Le Maître <eric.erwan.le.maitre@gmail.com>
2025-02-05 12:34:03 +0100

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'vue-ion-nav',
+  webDir: 'dist'
+};
+
+export default config;
